1c8c610be8b3ce9856468cb0c42c9d9037f40ce6
SirPudwhip <spamfolder3399@gmail.com>
2023-04-26 09:57:39 -0400

    added the user class to the backend + some authentication rules

--- server/app.py
+++ server/app.py
@@ -4,11 +4,13 @@ from flask_restful import Api, Resource
 from flask_media import Media
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from models import db
+# from flask_bcrypt import bcrypt
 
 
-# from models import 
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['MEDIA_PATH'] = '../server/videos'
@@ -17,19 +19,30 @@ app.config['MEDIA_DEFAULT_ENDPOINT'] = 'default'
 app.config['MEDIA_DEFAULT_URL'] = 'https://ih1.redbubble.net/image.2924701484.4363/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
 app.json.compact = False
 
-metadata = MetaData()
 
-db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
-
+ 
 api = Api(app)
 
-class HelloWorld(Resource):
+class Home(Resource):
     def get(self):
-        return jsonify({'message': 'Hello, World!'})
+        return jsonify({'message': 'Welcome to my shitty website '})
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Home, '/')
+
+class
+
+class Uploader(Resource): 
+    def post(self):
+        file = request.files['file']
+        upload = Video()
+        upload.media_file.save(file)
+        db.session.add(upload)
+        db.session.commit
+        return make_response({'The request went through':'maybe it saved? Who knows'})
+
+api.add_resource(Uploader, '/uploader')
 
 
 if __name__ == '__main__':
---
+++ server/migrations/versions/3345c2dbb7a0_initial_test.py
@@ -0,0 +1,24 @@
+"""initial test
+
+Revision ID: 3345c2dbb7a0
+Revises: 
+Create Date: 2023-04-25 15:45:15.279116
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3345c2dbb7a0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
