020e98b181420bfeb3f505a744223276d4bc9824
SirPudwhip <spamfolder3399@gmail.com>
2023-04-26 11:31:09 -0400

    added routes for check and clear session, making new users, as well as logout and login

--- server/app.py
+++ server/app.py
@@ -4,8 +4,9 @@ from flask_restful import Api, Resource
 from flask_media import Media
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from models import db
-# from flask_bcrypt import bcrypt
+from models import db, User
+from flask_bcrypt import Bcrypt
+
 
 
 
@@ -22,7 +23,7 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
- 
+bcrypt = Bcrypt(app)
 api = Api(app)
 
 class Home(Resource):
@@ -31,7 +32,72 @@ class Home(Resource):
 
 api.add_resource(Home, '/')
 
-class
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        email = data['email']
+        password = data['password']
+        user = User.query.filter(User.email == email).first()
+
+        if user is None:
+            return make_response({"error": "no user found with that email"}, 404)
+        if not bcrypt.check_password_hash(user._password_hash, password):
+            return make_response({"error": "Incorrect password"}, 404)
+
+        session.permanent = True
+        session[user.id] = user.id
+            
+        return make_response({
+            'username': user.username,
+            'email': user.email
+        }, 200)
+        
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session['user_id']
+        if user_id: 
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+
+        return make_response({}, 404)
+
+class ClearSession(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({}, 204)
+
+
+class CreateUser(Resource):
+    def get(self): 
+        return make_response({'message': 'create user resource works'})
+
+    def post(self): 
+        data = request.get_json()
+        username = data['username']
+        email = data['email']
+        password = data['password']
+        confirmation_pw = data['confirmation_pw']
+
+        exist = User.query.filter(User.email == email).first() is not None
+
+        if exist: 
+            return make_response({'error': 'that user is already in the system'}, 409)
+
+        hashed_password = bcrypt.generate_password_hash(password)
+        hashed_password_confirmation = bcrypt.generate_password_hash(confirmation_pw)
+
+        new_user = User(username = username, email=email, _password_hash = hashed_password, confirmation_pw = hashed_password_confirmation )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return make_response({
+            'id': new_user.id,
+            'email': new_user.email,
+        }, 204)
+        
+api.add_resource(CreateUser, '/createuser')
 
 class Uploader(Resource): 
     def post(self):
--- server/models.py
+++ server/models.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import validates
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.hybrid import hybrid_property
-from flask_bcrypt import bcrypt
+
 
 db = SQLAlchemy()
+
 
 class Video(db.Model, SerializerMixin):
     __tablename__ = 'videos'
@@ -36,7 +37,7 @@ class User(db.Model):
 
     def authenticate(self, password): 
         return bcrypt.check_password_hash(
-            self._password_has, password.encode('utf-8')
+            self._password_hash, password.encode('utf-8')
         )
 
     @staticmethod
